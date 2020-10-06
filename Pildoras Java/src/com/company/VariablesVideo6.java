15c7bf87cdab8af8d19b65ffa4786f990ce7ff98
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-06 18:24:35 -0500

    Variables. Close #6

---
+++ Pildoras Java/src/com/company/VariablesVideo6.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class VariablesVideo6 {
+
+    public static void main(String[] args) {
+
+        int edad = 30;
+        System.out.println(edad);
+
+        /*No es necesario indicar ese tipo de dato pero por estandar es lo ideal*/
+        edad = 75;
+        System.out.println(edad);
+
+    }
+
+}
