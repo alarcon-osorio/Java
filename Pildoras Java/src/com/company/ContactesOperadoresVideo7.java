c0b313c7df399e2b860eff6ab031f821f6f9e523
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-06 18:30:12 -0500

    Constantes y Operadores. Close #7

---
+++ Pildoras Java/src/com/company/ContactesOperadoresVideo7.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class ContactesOperadoresVideo7 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b;
+
+        b = 7;
+
+        int c = b + a ;
+
+        c++; //Incrementamos en uno
+        c--; //Decrementamos en uno
+        c+= 6; //Incrementamos en 6
+
+        System.out.println(c);
+
+    }
+}
