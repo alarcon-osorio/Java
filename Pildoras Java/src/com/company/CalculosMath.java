a94d958ff67dee38b5760b8c4ce12f327909e242
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-06 19:15:55 -0500

    Clase Math. Close #9

---
+++ Pildoras Java/src/com/company/CalculosMath.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class CalculosMath {
+
+    public static void main(String[] args) {
+
+        double raiz = Math.sqrt(7);
+
+        System.out.println("args = " + raiz);
+
+    }
+
+}
