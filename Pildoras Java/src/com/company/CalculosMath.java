c8c0a588908f0e502b35dfa7539598bc25ab72f2
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-06 19:25:19 -0500

    Method Pow, Round. Close #10

--- Pildoras Java/src/com/company/CalculosMath.java
+++ Pildoras Java/src/com/company/CalculosMath.java
@@ -7,8 +7,16 @@ public class CalculosMath {
     public static void main(String[] args) {
 
         double raiz = Math.sqrt(7);
+        System.out.println("Resultado = " + raiz);
 
-        System.out.println("args = " + raiz);
+        double num1 = 5.85;
+        long resultado = Math.round(num1);//devuelve long -- se puede castear con (int)Math.round(num1);
+        System.out.println("Resultado = " + resultado);
+
+        double base = 5;
+        double exponente = 3;
+        int resultadoPow = (int)Math.pow(base,exponente);
+        System.out.println("Resultado = " + resultadoPow);
 
     }
 
