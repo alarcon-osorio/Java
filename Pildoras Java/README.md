0bbcc22abdad510bab27ba26f698a4d2066d1c74
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-07 18:17:37 -0500

    Paquetes import. Close #13

---
+++ Pildoras Java/README.md
@@ -0,0 +1,5 @@
+# Pildoras Informaticas curso intensivo JAVA
+
+>> API - Java: 
+> 
+>Url: docs.oracle.com/javase/7/docs/api
---
+++ Pildoras Java/src/com/company/apiJavaVideo13.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner; // importada por la API de JAVA
+
+public class apiJavaVideo13 {
+
+    public static void main(String[] args) {
+        String nombre;
+        Scanner miObjeto; //Scanner de java.util importado
+    }
+
+}
