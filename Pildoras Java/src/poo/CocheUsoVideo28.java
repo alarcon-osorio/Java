58151823216d5f2399d07e532c76e8d533a1a0e4
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-13 19:07:16 -0500

    POO Practico. Close #28

--- Pildoras Java/src/poo/CocheUsoVideo28.java
+++ Pildoras Java/src/poo/CocheUsoVideo28.java
@@ -1,2 +1,11 @@
-package poo;public class CocheUsoVideo28 {
+package poo;
+
+public class CocheUsoVideo28 {
+
+    public static void main(String[] args) {
+        //Instanciamos la clase
+        CocheVideo28 Renault = new CocheVideo28(); //Ejemplar de Clase
+        System.out.println("el Coche tiene: " + Renault.ruedas + " ruedas");
+    }
+
 }
--- Pildoras Java/src/poo/CocheVideo28.java
+++ Pildoras Java/src/poo/CocheVideo28.java
@@ -1,2 +1,20 @@
-package poo;public class CocheVideo28 {
+package poo;
+
+public class CocheVideo28 {
+
+    int ruedas;
+    int largo;//cm
+    int ancho;//cm
+    int motor;//cm3
+    int peso;//kg - entero
+
+    //Crear metodo constructor (Inicializamos objetos)
+    public CocheVideo28(){
+        ruedas = 4;
+        largo = 200;
+        ancho = 300;
+        motor = 1600;
+        peso = 500;
+    }
+
 }
