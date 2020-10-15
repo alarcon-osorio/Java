8cb2c2f7f91f508b200a977a10e2960987c7b76e
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-14 20:04:51 -0500

    POO VI - Construccion de Objetos. Close #32

---
+++ Pildoras Java/src/poo/CocheUsoVideo28_29_30_31_32.java
@@ -0,0 +1,30 @@
+package poo;
+
+import javax.swing.*;
+import java.util.Arrays;
+
+public class CocheUsoVideo28_29_30_31_32 {
+
+    public static void main(String[] args) {
+        //Instanciamos la clase
+        CocheVideo28_29_30_31_32 Renault = new CocheVideo28_29_30_31_32(); //Ejemplar de Clase Video 30
+        //Renault.ruedas = 3; // No modificar desde aqui esas propiedades, con esto se aplica encapsulacion(anota error ruedas)
+        //Metodos Getters y Setters Video 30
+        System.out.println(Renault.obtenerDatos());
+        //Se llama desde la clase de Coche
+        //Renault.estableceColor(); Video 31 arroja error
+        Renault.establecerColor(JOptionPane.showInputDialog("Introduce Color"));
+        //Renault.color = "Rosa";  //-- Video 31 Problema de concepto encapsulacion marcar private
+        System.out.println(Renault.obtenerColor());
+        //Video 31 -->Envio parametros
+        Renault.establecerAsientos(JOptionPane.showInputDialog("Tiene Asientos de cuero"));
+        System.out.println(Renault.obtenerAsientos());
+        //Video32
+        Renault.establecerClimatizador(JOptionPane.showInputDialog("Tiene Aire acondicionado"));
+        System.out.println(Renault.obtenerClimatizador());
+        System.out.println(Renault.obtenerPesoCoche());
+        System.out.println("El precio del coche es: $" + Renault.precioCoche());
+
+    }
+
+}
--- Pildoras Java/src/poo/CocheVideo28_29_30_31_32.java
+++ Pildoras Java/src/poo/CocheVideo28_29_30_31_32.java
@@ -1,6 +1,6 @@
 package poo;
 
-public class CocheVideo28_29_30_31 {
+public class CocheVideo28_29_30_31_32 {
 
     private int ruedas;//con Private encapsulo esa propiedad
     private int largo;//cm
@@ -8,11 +8,11 @@ public class CocheVideo28_29_30_31 {
     private int motor;//cm3
     private int peso;//kg - entero
     private String color;
-    private int peso_total;
+    private int pesoTotal;
     private boolean asientosCuero, climatizador;
 
     //Crear metodo constructor (Inicializamos objetos)
-    public CocheVideo28_29_30_31(){
+    public CocheVideo28_29_30_31_32(){
         ruedas = 4;
         largo = 2000;
         ancho = 3000;
@@ -41,15 +41,17 @@ public class CocheVideo28_29_30_31 {
         return "El color del coche es " + color;
     }
 
+    //Video 31
     //Setter con parametro
     public void establecerAsientos(String asientosCuero) {
-        if(asientosCuero == "si") {
+        if(asientosCuero.equalsIgnoreCase("si")) {
             this.asientosCuero = true;
         }else{
             this.asientosCuero = false;
         }
     }
 
+    //Video 31
     //Getter para el Setter de arriba
     public String obtenerAsientos() {
         if(asientosCuero == true){
@@ -58,6 +60,56 @@ public class CocheVideo28_29_30_31 {
             return "No tiene asientos de cuero";
         }
 
+    }
+
+    //Video32
+    //Setter
+    public void establecerClimatizador(String climatizador){
+        if (climatizador.equalsIgnoreCase("si")){
+            this.climatizador = true;
+        }else{
+            this.climatizador = false;
+        }
+    }
+
+    //Video32
+    //Getter
+    public String obtenerClimatizador(){
+        if (climatizador==true){
+            return "Tiene aire acondicionado";
+        }else{
+            return "No tiene aire acondiconado";
+        }
+    }
+
+    //Video32 Seter con Getter a la vez desaconsejado
+    public String obtenerPesoCoche(){
+        int pesoCarroceria = 500;
+        pesoTotal = pesoTotal + pesoCarroceria;
+
+        if(asientosCuero==true){
+            pesoTotal=pesoTotal + 50;
+        }
+
+        if(climatizador==true){
+            pesoTotal=pesoTotal + 20;
+        }
+
+        return "El peso del coche es " + pesoTotal;
+    }
+
+    //Video 32 Getter
+    public int precioCoche(){
+        int precioFinal = 10000;
+
+        if (asientosCuero = true){
+            precioFinal += 2000;
+        }
+        if (climatizador==true){
+            precioFinal += 5000;
+        }
+
+        return precioFinal;
     }
 
 
