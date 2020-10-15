f36d123533ee7a606f1faf912f559bb14fccdc69
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-15 18:49:36 -0500

    Constructores II. Close #34

--- Pildoras Java/src/poo/EmpleadoUso.java
+++ Pildoras Java/src/poo/EmpleadoUso.java
@@ -1,6 +1,6 @@
 package poo;
 
-import java.util.Date;
+import java.util.*;
 
 public class EmpleadoUso {
 
@@ -12,13 +12,51 @@ public class EmpleadoUso {
 
 //Video 33 -- solo una clase debe ser publica y solo una debe ser main
 class Empleado{
-    // Constructor con parametros
-    public Empleado(String name, double billing, int year, int mes, int dia) {
-
-    }
-
     private String name; // Tipos de objetos
     private double billing;  // Tipos primitivos
     private Date altaContrato; // Tipos de objetos
+
+    // Constructor con parametros
+    //Video 34 - uso de GreforianCalendar
+    public Empleado(String name, double billing, int year, int mes, int dia) {
+        this.name = name;
+        this.billing = billing;
+        GregorianCalendar calendaro = new GregorianCalendar(year, mes -1, dia); //Video 33 - Enero es 1 - 1 = 0 para controlarlo
+        altaContrato =  calendaro.getTime(); //Video 33 - Intro Herencia
+    }
+
+    //Video 34 - Metodo Getter
+    public String obtenerNombre(){
+        return name;
+    }
+
+    //Video 34 - Metodo Getter
+    public double obtenerSueldo(){
+        return billing;
+    }
+
+    //Video 34 - Metodo Getter
+    public Date obtenerFechaContrato(){
+        return altaContrato;
+    }
+
+    //Video 34 - Meotodo Setter
+    public void estableNombre(){
+
+    }
+
+    //Video 34 - Meotodo Setter
+    public void estableSueldo(double porcentaje){
+        double aumento = billing * porcentaje/100;
+        billing += aumento;
+    }
+
+    //Video 34 - Meotodo Setter
+    public void estableFechaContrato(){
+
+    }
+
+
+
 
 }
