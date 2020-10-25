7c4c2546715eb3972c0f24a997cda2d3f112bf94
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-24 19:01:20 -0500

    Clases Internas II. Close #54

--- Pildoras Java/src/poo/TemporizadorII.java
+++ Pildoras Java/src/poo/TemporizadorII.java
@@ -9,8 +9,8 @@ import javax.swing.Timer;
 //Video 53 - Clases Internas I
 public class TemporizadorII {
     public static void main(String[] args) {
-        Reloj reloj = new Reloj(3000, true);
-        reloj.Marcha();
+        Reloj reloj = new Reloj(); //Error ya que no existe se quitan parametros 3000, true y se estableven en marcha
+        reloj.Marcha(3000, true);
         JOptionPane.showMessageDialog(null, "Pulsar Aceptar para terminar");
         System.exit(0);
     }
@@ -21,18 +21,34 @@ class Reloj{
     private int intervalo;
     private boolean sonido;
 
+    /***** Se comenta Video 54
     public Reloj(int intervalo, boolean sonido){
         this.intervalo = intervalo;
         this.sonido = sonido;
 
-    }
+    } *******/
 
-    public void Marcha(){
+    public void Marcha(int intervalo, final boolean sonido){ //Video 54 - Clases Internas II --- Se llaman las variables para simplificar codigo, modificador final para acceder a variable local
+
+        //Video 54 - Clases internas II --- Se mueve la clase dentro del motodo
+        class GetHour2 implements ActionListener{ //Modificar de acceso obligatorio para clases internas  // Video 54 - Clases Internas II --- Quitar modificar de acceso private
+            public void actionPerformed(ActionEvent e){
+                Date date = new Date();
+                System.out.println("Hora cada 3 segundos= " + date);
+
+                if (sonido){
+                    Toolkit.getDefaultToolkit().beep();  //Sonido de Windows
+                }
+
+            }
+        }
+
         ActionListener oyente = new GetHour2();
         Timer temporizador = new Timer(2000, oyente);
         temporizador.start();
     }
 
+    /***** Se comenta para que se traslade al metodo Video 54
     private class GetHour2 implements ActionListener{ //Modificar de acceso obligatorio para clases internas
         public void actionPerformed(ActionEvent e){
             Date date = new Date();
@@ -43,6 +59,6 @@ class Reloj{
             }
 
         }
-    }
+    }**********/
 
 }
