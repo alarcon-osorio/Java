bc642994717c317c08c668f50e9b3777102261d4
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-26 18:22:25 -0500

    Swing II. Close #56

--- Pildoras Java/src/graficos/Frames.java
+++ Pildoras Java/src/graficos/Frames.java
@@ -13,6 +13,11 @@ public class Frames {
 
 class myFrame extends JFrame{
     public myFrame(){
-        setSize(400,400);
+        //setSize(400,400);  //Tamaño
+        //Video 56 - Swing II - Aplicaciones graficas con swing
+        //setLocation(500,300);  //Ubicacion -  Video 56
+        setBounds(500,300,250,250);  //Ubicacion y Tamaño -  Video 56
+        //setResizable(false); //Redimension -  Video 56
+        setExtendedState(myFrame.MAXIMIZED_BOTH);// MARCO PANTALL COMPLETA -  Video 56
     }
 }
