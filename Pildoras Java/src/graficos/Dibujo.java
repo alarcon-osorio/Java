c339b2c9751f56f65c8262856332ce49f786c242
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-26 19:39:33 -0500

    Swing JAVA 2D. Close #60

--- Pildoras Java/src/graficos/Dibujo.java
+++ Pildoras Java/src/graficos/Dibujo.java
@@ -2,6 +2,10 @@ package graficos;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
 //Video 59 - Swing V - Frame V Graficos con Java - Dibujos
 public class Dibujo {
     public static void main(String[] args) {
@@ -23,6 +27,20 @@ class MarcoConDibujos extends JFrame{
 class LaminaConFiguras extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawRect(200,200,200,200);
+        //g.drawRect(200,200,200,200); //Video 60 -- Comentario
+        //Video 60 - Swing VI Java 2D Graphics
+        Graphics2D graphics2D = (Graphics2D) g;
+        Rectangle2D rectangulo = new Rectangle2D.Double(100,100,200,150);
+        graphics2D.draw(rectangulo);
+        Ellipse2D ellipse2D = new Ellipse2D.Double();
+        ellipse2D.setFrame(rectangulo);
+        graphics2D.draw(ellipse2D);
+        graphics2D.draw(new Line2D.Double(100,100,300,250));
+        double CentroenX = rectangulo.getCenterX(); //Centro en X del rectangulo
+        double CentroenY = rectangulo.getCenterY(); //Centro en Y del rectangulo
+        double radio = 150;
+        Ellipse2D circulo = new Ellipse2D.Double();
+        circulo.setFrameFromCenter(CentroenX,CentroenY,CentroenX + radio, CentroenY+radio);
+        graphics2D.draw(circulo);
     }
 }
